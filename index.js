0c25bd7b44c7095460f3aabf325e4183478fae83
HenryGunadi <henrywilliamgunadi09@gmail.com>
2025-10-21 16:57:36 +0700

    nambah fungsi set random ikan dan sampah di canvas

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import * as henryFunctions from "./2372071.js"
+
 var cnv = document.querySelector("#myCanvas");
 var ctx;
 ctx = cnv.getContext("2d");
@@ -5,6 +7,7 @@ var imageData = ctx.getImageData(10, 0, cnv.width, cnv.height);
 
 function main() {
     // Kode Main
+    henryFunctions.generateFishAndTrash(imageData, cnv, 20, 10, {r: 0, g: 0, b: 255}, {r: 255, g: 0, b: 0})
 
     ctx.putImageData(imageData, 0, 0);
 }
